1e2bcf453d7131b3d6ee6ed5d4266b01b55e8176
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-24 16:57:31 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+
+{
+	cout << "\nLab2.3\n";
+	int x, y;
+	x = 45;
+	y = -10;
+
+	//if x < 5 y >= -10 - отсутсвуют скобки для условия и нету логического оператора
+	//printf(" % d", x + y);
+
+	if (x < 5) {
+		if (y >= -10)
+			printf("b) %d", x + y);// правильно
+	}
+
+	if (x < 5, y >= -10)
+		printf("c) % d", x + y);// правильно
+
+	if (x < 5 && y >= -10)
+		printf("d) % d", x + y);// правильно
+	// эквивалентны между собой b,d,c
+}
