4955d70ff9680987f711120940a05c6c99a34902
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-24 16:50:55 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -5,23 +5,8 @@ using namespace std;
 int main()
 
 {
-	cout << "\nLab2.3\n";
-	int x, y;
-	x = 45;
-	y = -10;
+	/*
+	 if/else, while, do/while, for
+	*/
 
-	//if x < 5 y >= -10 - отсутсвуют скобки для условия и нету логического оператора
-	//printf(" % d", x + y);
-
-	if (x < 5) {
-		if (y >= -10)
-			printf("b) %d", x + y);// правильно
-	}
-
-	if (x < 5, y >= -10)
-		printf("c) % d", x + y);// правильно
-
-	if (x < 5 && y >= -10)
-		printf("d) % d", x + y);// правильно
-	// эквивалентны между собой b,d,c
 }
