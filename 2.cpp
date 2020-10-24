cf4233165484283d6ea2560f5ac2dab382d24321
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-24 16:54:54 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,27 +1,41 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 
 using namespace std;
 int main()
 
 {
-	cout << "\nLab2.3\n";
-	int x, y;
-	x = 45;
-	y = -10;
+/*
+1)
+if (e1) 
+   if (e2) S1; 
+   else S2;
+   
+2)
+if (e1) 
+    { 
+    if (e2) S1; 
+    else S2; 
+    }
+    
+3)
+if (e1) 
+    { 
+    if (e2) S1; 
+    } 
+else S2;
 
-	//if x < 5 y >= -10 - отсутсвуют скобки для условия и нету логического оператора
-	//printf(" % d", x + y);
+4)
+if (e1) 
+    if (e2) S1; 
+    else ; 
+else S2;
 
-	if (x < 5) {
-		if (y >= -10)
-			printf("b) %d", x + y);// правильно
-	}
-
-	if (x < 5, y >= -10)
-		printf("c) % d", x + y);// правильно
-
-	if (x < 5 && y >= -10)
-		printf("d) % d", x + y);// правильно
-	// эквивалентны между собой b,d,c
+5)
+if (e1) 
+    if (e2) S1; 
+    else S2; 
+else ;
+*/
+	
 }
