04ce7c76e98a15e88e27e43ed240fa4108816557
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-24 17:49:39 +0300

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,9 +1,14 @@
-﻿#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <cmath>
 
-using namespace std;
 int main()
-
 {
-	
+	int A, B, C;
+	printf("Vvedite A,B,C\n");
+	scanf("%d", &A);
+	scanf("%d", &B);
+	scanf("%d", &C);
+	if ((A % 2 == 0) || (B % 2 == 0) || (C % 2 == 0)) printf("\nYes");
+	else printf("No");
 }
