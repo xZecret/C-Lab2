a6ead291f7cfd7c5f3002b359fe5474e3363c956
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-24 18:01:35 +0300

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    int e, f;
+    printf("[e, f] = ");
+    scanf("%d%d", &e, &f);
+    if (e < f)
+        for (int i = 0; i < 3; i++)
+        {
+            printf("%c = ", 'a' + i);
+            int j;
+            scanf("%d", &j);
+            printf((e <= j && j <= f) ? "true\n" : "false\n");
+        }
+    return 0;
+}
